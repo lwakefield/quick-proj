d4169b54ebcc6578edb5028bfc4e366bae4dc3c3
Lawrence Wakefield <lawrence@iamlawrence.me>
2016-04-08 09:51:15 -0400

    flattened the build for gh-pages

--- config.js
+++ config.js
@@ -4,8 +4,8 @@ module.exports = {
   build: {
     index: path.resolve(__dirname, 'dist/index.html'),
     assetsRoot: path.resolve(__dirname, 'dist'),
-    assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsSubDirectory: '',
+    assetsPublicPath: '',
     productionSourceMap: true
   },
   dev: {
