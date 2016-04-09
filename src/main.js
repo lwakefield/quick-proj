cbcd6e8e1e76215b49f0d2e5ebe1dbcbfa43c6f8
Lawrence Wakefield <lawrence@iamlawrence.me>
2016-04-09 11:23:32 -0400

    added FirebaseOffline, maintains a synced cache when online and a dirty cache when offline

--- src/main.js
+++ src/main.js
@@ -1,5 +1,79 @@
 import Vue from 'vue';
 import App from './App';
+import Firebase from 'firebase';
+import {
+    objectSet,
+    objectGet
+} from 'jshelpme';
+import generatePushID from './generatePushID';
+
+/* eslint-disable no-unused-vars */
+class Store {
+    static get(key) {
+        let path = key.split('.');
+        let parentPath = path[0];
+        let childPath = path.splice(1).join('.');
+        let item = JSON.parse(localStorage[parentPath] || '{}');
+        return objectGet(item, childPath);
+    }
+    static set(key, val) {
+        let path = key.split('.');
+        let parentPath = path[0];
+        let childPath = path.splice(1).join('.');
+        let item = JSON.parse(localStorage[parentPath] || '{}');
+        objectSet(item, childPath, val);
+        localStorage[parentPath] = JSON.stringify(item);
+    }
+}
+
+class FirebaseOffline extends Firebase {
+
+    constructor(url, prefix = 'offline') {
+        super(url);
+        // builds the path, strips the protocol and removes empty strings
+        // https://my-app-name.firebaseio.com/foo/1234/bar
+        //   => app = 'my-app-name'
+        //   => path = 'foo.1234.bar'
+        let urlSplit = url.split('/').filter((v, i) => i > 0 && v);
+        let app = urlSplit[0].split('.')[0];
+        let path = urlSplit.splice(1).join('.');
+
+        this._app = app;
+        this._path = path;
+        this._prefix = prefix;
+
+        this.on('value', snapshot => {
+            Store.set(`${this._prefix}_${this._app}.${this._path}`, snapshot.val());
+        });
+
+        this.root().child('.info/connected').on('value', snapshot => {
+            this.online = snapshot.val();
+        });
+    }
+
+    // child, parent
+    // set, update, remove, push
+
+    set(value, onComplete) {
+        if (this.online) super.set(value, onComplete);
+        else Store.set(`${this._prefix}_dirty_${this._app}.${this._path}`, value);
+    }
+
+    push(value, onComplete) {
+        if (this.online) {
+            super.push(value, onComplete);
+        } else {
+            let pushID = generatePushID();
+            Store.set(`${this._prefix}_dirty_${this._app}.${this._path}.${pushID}`, value);
+        }
+    }
+
+}
+
+let fo = new FirebaseOffline('https://vivid-torch-9375.firebaseio.com/foo/1234/bar');
+window.fo = fo;
+window.FirebaseOffline = FirebaseOffline;
+window.Store = Store;
 
 require('offline-plugin/runtime').install();
 
